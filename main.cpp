79fb4579659f63c2004ba8c9655ad020c0017f36
Konstantin Vlasov <support@flint-inc.ru>
2018-04-30 18:06:20 +0300

    Added support for hostname masks

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <Winternl.h>
 
 #ifdef _UNICODE
 #if defined _M_IX86
@@ -12,11 +13,43 @@
 #endif
 #endif
 
+#pragma comment(lib, "ntdll.lib")
+
 const wchar_t* AppName = L"Browser Selector";
 const size_t BUF_SIZE = 32768;
 
+extern "C" BOOLEAN WINAPI RtlIsNameInExpression(
+	PUNICODE_STRING Expression,
+	PUNICODE_STRING Name,
+	BOOLEAN         IgnoreCase,
+	PWCH            UpcaseTable
+);
+extern "C" NTSTATUS WINAPI RtlUpcaseUnicodeString(
+	PUNICODE_STRING DestinationString,
+	PCUNICODE_STRING SourceString,
+	BOOLEAN AllocateDestinationString
+);
+
+
+bool Match(const wchar_t* HostName, const wchar_t* Mask)
+{
+	UNICODE_STRING uHostName, uMask, uMaskUC;
+	RtlInitUnicodeString(&uHostName, HostName);
+	RtlInitUnicodeString(&uMask, Mask);
+	RtlUpcaseUnicodeString(&uMaskUC, &uMask, TRUE);
+	BOOLEAN res = RtlIsNameInExpression(&uMaskUC, &uHostName, TRUE, NULL);
+	RtlFreeUnicodeString(&uMaskUC);
+	return (res != FALSE);
+}
+
 int WINAPI wWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPWSTR lpCmdLine, int /*nCmdShow*/)
 {
+#if 0
+	bool r;
+	r = Match(L".tinkoff.ru", L"*.tinkoff.ru");
+	MessageBox(NULL, r ? L"+" : L"-", L"", MB_OK);
+	return 0;
+#endif
 	if (lpCmdLine[0] == L'\0')
 	{
 		MessageBox(NULL, L"Usage: BrowserSelector.exe <URL>", AppName, MB_ICONINFORMATION | MB_OK);
@@ -55,22 +88,49 @@ int WINAPI wWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPWSTR
 	}
 	hostStart += 3;
 	const wchar_t* hostEnd = wcsstr(hostStart, L"/");
+	size_t hostLen;
 	if (hostEnd == NULL)
-		wcscpy_s(buf1, BUF_SIZE, hostStart);
+		hostLen = wcslen(hostStart) - 1;
 	else
-		wcsncpy_s(buf1, BUF_SIZE, hostStart, hostEnd - hostStart);
+		hostLen = hostEnd - hostStart;
+	wcsncpy_s(buf1, BUF_SIZE, hostStart, hostLen);
 	// Now search for this hostname in our INI file, section [Hostnames]
-	DWORD res = GetPrivateProfileString(L"Hostnames", buf1, NULL, buf2, BUF_SIZE, iniFile);
-	if ((res == 0) || (buf2[0] == L'\0'))
+	wchar_t* allHostnames = new wchar_t[32767];
+	if (GetPrivateProfileSection(L"Hostnames", allHostnames, 32767, iniFile) > 0)
 	{
-		// Not found, taking the defalut browser
-		res = GetPrivateProfileString(L"General", L"DefaultBrowser", NULL, buf2, BUF_SIZE, iniFile);
-		if ((res == 0) || (buf2[0] == L'\0'))
+		wchar_t* hostmaskLine = allHostnames;
+		bool found = false;
+		// INI section lines are 0-byte-delimited, and the whole section ends with two 0-bytes
+		while (*hostmaskLine != L'\0')
 		{
-			MessageBox(NULL, L"Failed to determine the browser!", AppName, MB_ICONERROR | MB_OK);
-			return 1;
+			size_t len = wcslen(hostmaskLine);  // Length of the current line
+			wchar_t* pos = wcschr(hostmaskLine, L'=');
+			if (pos != NULL)
+			{
+				*pos = L'\0';
+				if (Match(buf1, hostmaskLine))
+				{
+					// Found match
+					found = true;
+					wcscpy_s(buf2, BUF_SIZE, pos + 1);
+					break;
+				}
+			}
+			// Move to the next line
+			hostmaskLine += len + 1;
+		}
+		if (!found)
+		{
+			// Not found, taking the defalut browser
+			DWORD res = GetPrivateProfileString(L"General", L"DefaultBrowser", NULL, buf2, BUF_SIZE, iniFile);
+			if ((res == 0) || (buf2[0] == L'\0'))
+			{
+				MessageBox(NULL, L"Failed to determine the browser!", AppName, MB_ICONERROR | MB_OK);
+				return 1;
+			}
 		}
 	}
+	delete[] allHostnames;
 	// Apply hacks: replacing HTTPS with HTTP for specified sites
 	const size_t MAX_PROTO_LEN = 16;
 	wchar_t proto[MAX_PROTO_LEN];
@@ -79,7 +139,7 @@ int WINAPI wWinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPWSTR
 	else
 		wcsncpy_s(proto, lpCmdLine + 1, hostStart - lpCmdLine - 4);
 	// Get the executable for the chosen browser
-	res = GetPrivateProfileString(buf2, L"Browser", NULL, buf1, BUF_SIZE, iniFile);
+	DWORD res = GetPrivateProfileString(buf2, L"Browser", NULL, buf1, BUF_SIZE, iniFile);
 	if ((res == 0) || (buf1[0] == L'\0'))
 	{
 		MessageBox(NULL, L"Failed to determine the browser executable!", AppName, MB_ICONERROR | MB_OK);
